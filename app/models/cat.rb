7c65ea49f12dd3eec7c8bcbe74e966c4c6a92f45
joeseiden <joe.seiden214@gmail.com>
2017-02-14 11:51:04 -0800

    Create index/show pages

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,6 +1,20 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :integer          not null, primary key
+#  birth_date  :date             not null
+#  color       :string           not null
+#  name        :string           not null
+#  sex         :string(1)        not null
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Cat < ActiveRecord::Base
   validates :birth_date, :color, :name, :sex, presence: true
-  validates :color, inclusion: { in: %w(black white calico),
+  validates :color, inclusion: { in: %w(black white grey calico),
     message: "%{value} is not a valid color" }
   validates :sex, inclusion: { in: %w(M F),
     message: "%{value} is not a valid sex" }
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+c1 = Cat.create!(birth_date: '2014/3/14', name: 'Tina', color: 'grey', sex: 'F', description: 'RIP Tina')
+c2 = Cat.create!(birth_date: '2013/4/14', name: 'Beyonce', color: 'black', sex: 'F', description: 'Queen B')
+c3 = Cat.create!(birth_date: '2012/5/14', name: 'Jay Z', color: 'white', sex: 'M', description: 'Ma Man')
+c4 = Cat.create!(birth_date: '2011/6/14', name: 'MJ', color: 'calico', sex: 'M', description: 'RIP MJ')
